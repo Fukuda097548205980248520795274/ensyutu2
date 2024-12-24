e91d46c9a7398286dc4a2264037027fe2a780568
k024g0083 <k024g0083@g.neec.ac.jp>
2024-12-24 21:22:36 +0900

    Raindropsクラスを作成した

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <Novice.h>
+#include <stdlib.h>
+#include <time.h>
 #include "Constant.h"
 #include "./Class/Object/Player/Player.h"
 #include "./Class/Object/Particle/Rain/Rain.h"
+#include "./Class/Object/Particle/Raindrops/Raindrops.h"
 
 const char kWindowTitle[] = "LC1B_20_フクダソウワ";
 
 int Rain::countId;
+int Raindrops::countId;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -17,6 +21,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	// 乱数
+	unsigned int currentTimer = static_cast<unsigned int>(time(nullptr));
+	srand(currentTimer);
+
 
 	/*---------------
 	    変数を作る
@@ -24,6 +32,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// プレイヤー
 	Player* player = new Player();
+
+	// 雨
+	Rain* rain[kRainNum];
+	for (int i = 0; i < kRainNum; i++)
+	{
+		rain[i] = new Rain();
+	}
 
 	// 白い図形
 	int ghWhite = Novice::LoadTexture("./NoviceResources/white1x1.png");
@@ -46,6 +61,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		player->Move(keys, preKeys);
 		player->Jump(keys, preKeys);
 
+		// 雨
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < kRainNum; j++)
+			{
+				if (rain[j]->id_ == 0)
+				{
+					rain[j]->Emission(static_cast<float>(rand() % (kScreenWidth + 400)), 700.0f, 250.0f);
+
+					break;
+				}
+			}
+		}
+
+
+		for (int i = 0; i < kRainNum; i++)
+		{
+			rain[i]->Move();
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -61,6 +96,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// プレイヤー
 		player->Draw(ghWhite);
 
+		// 雨
+		for (int i = 0; i < kRainNum; i++)
+		{
+			rain[i]->Draw(ghWhite);
+		}
 
 		///
 		/// ↑描画処理ここまで
@@ -75,7 +115,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 	}
 
+
+	/*--------------------------
+	    インスタンスを削除する
+	--------------------------*/
+
+	// プレイヤー
 	delete player;
+
+	// 雨
+	for (int i = 0; i < kRainNum; i++)
+	{
+		delete rain[i];
+	}
 
 	// ライブラリの終了
 	Novice::Finalize();
